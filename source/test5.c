69083c8a6624c6a256a4b1d8cb157e545cb1e5e7
kjkl1111 <163836460+kjkl1111@users.noreply.github.com>
2024-03-19 12:22:46 +0900

    Add files via upload

---
+++ source/test5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int cnt;
+
+    printf("알파벳 대문자가 출력됩니다.\n");
+
+    for(cnt = 65; cnt <= 90; cnt++);
+ {
+    printf(" %c", cnt);
+}
+
+
+      return 0;
+}
